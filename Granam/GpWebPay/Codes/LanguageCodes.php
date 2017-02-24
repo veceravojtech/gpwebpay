7eea67da1a71cd176bd2b04fc7e756d48234071e
Jaroslav Týc <mail@jaroslavtyc.com>
2017-02-24 11:54:57 +0100

    Language codes extended by those available in GpWebPay backend

--- Granam/GpWebPay/Codes/LanguageCodes.php
+++ Granam/GpWebPay/Codes/LanguageCodes.php
@@ -11,25 +11,29 @@ use Granam\Strict\Object\StrictObject;
 class LanguageCodes extends StrictObject implements Codes
 {
     /** ISO 639-1 @link https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes */
-    const CS = 'cs'; // czech - čeština
-    const SK = 'sk'; // slovak - slovenčina
-    const EN = 'en'; // english
     const AR = 'ar'; // arabic - العربية
+    const CS = 'cs'; // czech - čeština
     const DA = 'da'; // danish - dansk
+    const NL = 'nl'; // dutch - nederlands
+    const EN = 'en'; // english (UK)
+    const EN_US = 'en_US'; // english (US)
     const FI = 'fi'; // finnish - suomi
     const FR = 'fr'; // french - français
-    const NL = 'nl'; // dutch - nederlands
-    const IT = 'it'; // italian - italiano
-    const LT = 'lt'; // lithuanian - lietuvių kalba
-    const HU = 'hu'; // hungarian - magyar
     const DE = 'de'; // german - deutsch
+    const HU = 'hu'; // hungarian - magyar
+    const IT = 'it'; // italian - italiano
+    const JA = 'ja'; // japanese - 日本語
+    const LV = 'lv'; // latvian - latviešu valoda
     const NO = 'no'; // norwegian - norsk
     const PL = 'pl'; // polish - polszczyzna
     const PT = 'pt'; // portuguese - português
+    const RO = 'ro'; // romanian - Română
     const RU = 'ru'; // russian - русский
+    const SK = 'sk'; // slovak - slovenčina
     const ES = 'es'; // spanish - español
     const SV = 'sv'; // swedish - svenska
     const UK = 'uk'; // ukrainian - українська
+    const VI = 'vi'; // vietnamese - Tiếng Việt
 
     /**
      * @return array|string[]
@@ -37,25 +41,29 @@ class LanguageCodes extends StrictObject implements Codes
     public static function getLanguageCodes()
     {
         return [
-            self::CS,
-            self::SK,
-            self::EN,
             self::AR,
+            self::CS,
             self::DA,
+            self::NL,
+            self::EN,
+            self::EN_US,
             self::FI,
             self::FR,
-            self::NL,
-            self::IT,
-            self::LT,
-            self::HU,
             self::DE,
+            self::HU,
+            self::IT,
+            self::JA,
+            self::LV,
             self::NO,
             self::PL,
             self::PT,
+            self::RO,
             self::RU,
+            self::SK,
             self::ES,
             self::SV,
             self::UK,
+            self::VI,
         ];
     }
 
