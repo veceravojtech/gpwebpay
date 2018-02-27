6212a568381f25df19f28710651389a85dd4b120
Jaroslav Týc <mail@jaroslavtyc.com>
2018-02-27 12:40:13 +0100

    PR codes are expressed by constants

--- Granam/GpWebPay/Codes/PrCodes.php
+++ Granam/GpWebPay/Codes/PrCodes.php
@@ -10,61 +10,84 @@ class PrCodes extends StrictObject implements Codes
     const INVALID_FIELD_CONTENT_CODE = 3;
     const ADDITIONAL_INFO_REQUEST_CODE = 200;
 
+    // ERROR CODES
+    const FIELD_IS_TOO_LONG = 1;
+    const FIELD_IS_TOO_SHORT = 2;
+    const FIELD_IS_EMPTY = 4;
+    const MISSING_REQUIRED_FIELD = 5;
+    const UNKNOWN_MERCHANT = 11;
+    const DUPLICATE_ORDER_NUMBER = 14;
+    const OBJECT_NOT_FOUND = 15;
+    const AMOUNT_TO_DEPOSIT_EXCEEDS_APPROVED_AMOUNT = 17;
+    const TOTAL_SUM_OF_CREDITED_AMOUNTS_EXCEEDED_DEPOSITED_AMOUNT = 18;
+    const OBJECT_NOT_IN_VALID_STATE_FOR_OPERATION = 20;
+    const OPERATION_NOT_ALLOWED_FOR_USER = 25;
+    const TECHNICAL_PROBLEM_IN_CONNECTION_TO_AUTHORIZATION_CENTER = 26;
+    const INCORRECT_ORDER_TYPE = 27;
+    const DECLINED_IN_3D_SECURE = 28;
+    const DECLINED_IN_AUTHORIZATION_CENTRE = 30;
+    const WRONG_DIGEST = 31;
+    const SESSION_EXPIRED = 35;
+    const DECLINED_DUE_TO_SUSPICION_OF_UNAUTHORIZED_USE_OF_A_CARD = 40;
+    const THE_CARDHOLDER_CANCELED_THE_PAYMENT = 50;
+    const TECHNICAL_PROBLEM = 1000;
+    const GENERIC_PROBLEM = 'genericProblem';
+
     private static $prCodes = [
         LanguageCodes::CS => [
-            'genericProblem' => 'Technický problém v GP webpay systému, kontaktujte obchodníka',
+            self::GENERIC_PROBLEM => 'Technický problém v GP webpay systému, kontaktujte obchodníka',
             self::OK_CODE => 'OK',
-            1 => 'Pole příliš dlouhé',
-            2 => 'Pole příliš krátké',
+            self::FIELD_IS_TOO_LONG => 'Pole příliš dlouhé',
+            self::FIELD_IS_TOO_SHORT => 'Pole příliš krátké',
             self::INVALID_FIELD_CONTENT_CODE => 'Chybný obsah pole',
-            4 => 'Pole je prázdné',
-            5 => 'Chybí povinné pole',
-            11 => 'Neznámý obchodník',
-            14 => 'Duplikátní číslo platby',
-            15 => 'Objekt nenalezen',
-            17 => 'Částka k zaplacení překročila povolenou (autorizovanou) částku',
-            18 => 'Součet vracených částek překročil zaplacenou částku',
-            20 => 'Objekt není ve stavu odpovídajícím této operaci
+            self::FIELD_IS_EMPTY => 'Pole je prázdné',
+            self::MISSING_REQUIRED_FIELD => 'Chybí povinné pole',
+            self::UNKNOWN_MERCHANT => 'Neznámý obchodník',
+            self::DUPLICATE_ORDER_NUMBER => 'Duplikátní číslo platby',
+            self::OBJECT_NOT_FOUND => 'Objekt nenalezen',
+            self::AMOUNT_TO_DEPOSIT_EXCEEDS_APPROVED_AMOUNT => 'Částka k zaplacení překročila povolenou (autorizovanou) částku',
+            self::TOTAL_SUM_OF_CREDITED_AMOUNTS_EXCEEDED_DEPOSITED_AMOUNT => 'Součet vracených částek překročil zaplacenou částku',
+            self::OBJECT_NOT_IN_VALID_STATE_FOR_OPERATION => 'Objekt není ve stavu odpovídajícím této operaci
 Info: Pokud v případě vytváření objednávky (CREATE_ORDER) obdrží obchodník tento návratový kód,
 vytvoření objednávky již proběhlo a objednávka je v určitém stavu
 – tento návratový kód je zapříčiněn aktivitou držitele karty (například pokusem o přechod zpět, použití refresh...)',
-            25 => 'Uživatel není oprávněn k provedení operace',
-            26 => 'Technický problém při spojení s autorizačním centrem',
-            27 => 'Chybný typ objednávky',
-            28 => 'Zamítnuto v 3D',
-            30 => 'Zamítnuto v autorizačním centru',
-            31 => 'Chybný podpis (digest)',
-            35 => 'Expirovaná session (nastává při vypršení webové session při zadávání karty)',
-            40 => 'Zamítnuto z podezření na neoprávněné použití platební karty',
-            50 => 'Držitel karty zrušil platbu',
+            self::OPERATION_NOT_ALLOWED_FOR_USER => 'Uživatel není oprávněn k provedení operace',
+            self::TECHNICAL_PROBLEM_IN_CONNECTION_TO_AUTHORIZATION_CENTER => 'Technický problém při spojení s autorizačním centrem',
+            self::INCORRECT_ORDER_TYPE => 'Chybný typ objednávky',
+            self::DECLINED_IN_3D_SECURE => 'Zamítnuto v 3D Secure',
+            self::DECLINED_IN_AUTHORIZATION_CENTRE => 'Zamítnuto v autorizačním centru',
+            self::WRONG_DIGEST => 'Chybný podpis (digest)',
+            self::SESSION_EXPIRED => 'Expirovaná session (nastává při vypršení webové session při zadávání karty)',
+            self::DECLINED_DUE_TO_SUSPICION_OF_UNAUTHORIZED_USE_OF_A_CARD => 'Zamítnuto z podezření na neoprávněné použití platební karty',
+            self::THE_CARDHOLDER_CANCELED_THE_PAYMENT => 'Držitel karty zrušil platbu',
             self::ADDITIONAL_INFO_REQUEST_CODE => 'Žádost o doplňující informace',
-            1000 => 'Technický problém',
+            self::TECHNICAL_PROBLEM => 'Technický problém',
         ],
         LanguageCodes::EN => [
-            'genericProblem' => 'Technical problem in GP webpay system, contact the merchant',
+            self::GENERIC_PROBLEM => 'Technical problem in GP webpay system, contact the merchant',
             self::OK_CODE => 'OK',
-            1 => 'Field too long',
-            2 => 'Field too short',
+            self::FIELD_IS_TOO_LONG => 'Field too long',
+            self::FIELD_IS_TOO_SHORT => 'Field too short',
             self::INVALID_FIELD_CONTENT_CODE => 'Incorrect content of field',
-            4 => 'Field is null',
-            5 => 'Missing required field',
-            11 => 'Unknown merchant',
-            14 => 'Duplicate order number',
-            15 => 'Object not found',
-            17 => 'Amount to deposit exceeds approved amount',
-            18 => 'Total sum of credited amounts exceeded deposited amount',
-            20 => 'Object not in valid state for operation',
-            25 => 'Operation not allowed for user',
-            26 => 'Technical problem in connection to authorization center',
-            27 => 'Incorrect order type',
-            28 => 'Declined in 3D',
-            30 => 'Declined in authorization centre',
-            31 => 'Wrong digest',
-            35 => 'Session expired (happens on web session expiration when entering a card)',
-            40 => 'Declined due to suspicion of unauthorized use of a card',
-            50 => 'The cardholder canceled the payment',
+            self::FIELD_IS_EMPTY => 'Field is null',
+            self::MISSING_REQUIRED_FIELD => 'Missing required field',
+            self::UNKNOWN_MERCHANT => 'Unknown merchant',
+            self::DUPLICATE_ORDER_NUMBER => 'Duplicate order number',
+            self::OBJECT_NOT_FOUND => 'Object not found',
+            self::AMOUNT_TO_DEPOSIT_EXCEEDS_APPROVED_AMOUNT => 'Amount to deposit exceeds approved amount',
+            self::TOTAL_SUM_OF_CREDITED_AMOUNTS_EXCEEDED_DEPOSITED_AMOUNT => 'Total sum of credited amounts exceeded deposited amount',
+            self::OBJECT_NOT_IN_VALID_STATE_FOR_OPERATION => 'Object not in valid state for operation',
+            self::OPERATION_NOT_ALLOWED_FOR_USER => 'Operation not allowed for user',
+            self::TECHNICAL_PROBLEM_IN_CONNECTION_TO_AUTHORIZATION_CENTER => 'Technical problem in connection to authorization center',
+            self::INCORRECT_ORDER_TYPE => 'Incorrect order type',
+            self::DECLINED_IN_3D_SECURE => 'Declined in 3D Secure',
+            self::DECLINED_IN_AUTHORIZATION_CENTRE => 'Declined in authorization centre',
+            self::WRONG_DIGEST => 'Wrong digest',
+            self::SESSION_EXPIRED => 'Session expired (happens on web session expiration when entering a card)',
+            self::DECLINED_DUE_TO_SUSPICION_OF_UNAUTHORIZED_USE_OF_A_CARD => 'Declined due to suspicion of unauthorized use of a card',
+            self::THE_CARDHOLDER_CANCELED_THE_PAYMENT => 'The cardholder canceled the payment',
             self::ADDITIONAL_INFO_REQUEST_CODE => 'Additional info request',
-            1000 => 'Technical problem',
+            self::TECHNICAL_PROBLEM => 'Technical problem',
         ],
     ];
 
@@ -112,6 +135,20 @@ vytvoření objednávky již proběhlo a objednávka je v určitém stavu
      */
     public static function isErrorForCustomer(int $prCode): bool
     {
-        return \in_array($prCode, [17, 25, 26, 28, 30, 35, 50, 40, 1000], true);
+        return \in_array(
+            $prCode,
+            [
+                self::AMOUNT_TO_DEPOSIT_EXCEEDS_APPROVED_AMOUNT,
+                self::OPERATION_NOT_ALLOWED_FOR_USER,
+                self::TECHNICAL_PROBLEM_IN_CONNECTION_TO_AUTHORIZATION_CENTER,
+                self::DECLINED_IN_3D_SECURE,
+                self::DECLINED_IN_AUTHORIZATION_CENTRE,
+                self::DECLINED_DUE_TO_SUSPICION_OF_UNAUTHORIZED_USE_OF_A_CARD,
+                self::SESSION_EXPIRED,
+                self::THE_CARDHOLDER_CANCELED_THE_PAYMENT,
+                self::TECHNICAL_PROBLEM
+            ],
+            true
+        );
     }
 }
